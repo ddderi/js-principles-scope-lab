901f84650347a1e22705f1778cce5f8ce205261b
ddderi <maxime830@live.fr>
2021-12-30 04:05:08 +0100

    commit tree b2c7902b204460fed97bcaaf3180ac1eb5f43ef9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const LeastFavoriteCustomer = 'Jean';
+
+function myFunction(){return customerName};
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+     LeastFavoriteCustomer = 'Mark';
+}
